70e633e1bed5ce12cbecbc55f946cbed1cce2bce
Sergey Balan <sergeyb@student.chalmers.se>
2022-01-06 17:52:42 +0100

    added comments

--- src/main/java/polestarbank/CustomerTransactionListController.java
+++ src/main/java/polestarbank/CustomerTransactionListController.java
@@ -25,73 +25,82 @@ import java.util.logging.Logger;
 
 public class CustomerTransactionListController implements Initializable {
 
+    // Establish connection to the database
+
     DatabaseConnector connectNow = new DatabaseConnector();
     Connection connectDB = connectNow.getConnection();
 
+    // FXML annotation, elements that we need from FXML file TransactionHistore.fxml and that are assigned to the SceneBuilder scene
+
     @FXML
-    private TableView<TransactionTable> transactionsTable;
+    private TableView<TransactionTable> transactionsTable; // the table
     @FXML
-    private TableColumn<TransactionTable, String> col_id;
+    private TableColumn<TransactionTable, String> col_id; // id number column
     @FXML
-    private TableColumn<TransactionTable, Date> col_date;
+    private TableColumn<TransactionTable, Date> col_date; // date column
     @FXML
-    private TableColumn<TransactionTable, Integer> col_sender;
+    private TableColumn<TransactionTable, Integer> col_sender; // sender account number column
     @FXML
-    private TableColumn<TransactionTable, Integer> col_amount;
+    private TableColumn<TransactionTable, Integer> col_amount; // amount column
     @FXML
-    private TableColumn<TransactionTable, Integer> col_payee;
+    private TableColumn<TransactionTable, Integer> col_payee; //payee account number column
     @FXML
-    private TextField keywordTextField;
+    private TextField keywordTextField; // search text field
 
 
-    ObservableList<TransactionTable> transactionsList = FXCollections.observableArrayList();
+    ObservableList<TransactionTable> transactionsList = FXCollections.observableArrayList();  // create Observablelist to allow LISTENER to track any changes
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        String transactionViewQuery = "SELECT * FROM Transcations;";
+        String transactionViewQuery = "SELECT * FROM Transcations;";  // the query to extract the data from the transactions table
         try {
-            Statement stm = connectDB.createStatement();
+            Statement stm = connectDB.createStatement();  // execute query
             ResultSet queryOutput = stm.executeQuery(transactionViewQuery);
-            while (queryOutput.next()) {
 
-                String queryId = queryOutput.getString("IDnumber");
+            while (queryOutput.next()) { // handle the query result
+
+                String queryId = queryOutput.getString("IDnumber");  // declare variables and initializing to the corresponding data type from database
                 Date queryDate = queryOutput.getDate("Date");
                 int querySenderAcc = queryOutput.getInt("Sender_account");
                 Integer queryAmount = queryOutput.getInt("Transfered_amount");
                 Integer queryPayeeAcc = queryOutput.getInt("Receiver_account");
 
 
-                transactionsList.add(new TransactionTable(queryId, queryDate,
+                transactionsList.add(new TransactionTable(queryId, queryDate, // populate the observable list with the data from the query result
                         querySenderAcc, queryAmount, queryPayeeAcc));
             }
 
+            //PropertyValueFactory corresponds to the new Transaction table fields that we annotate above as @FXML
             col_id.setCellValueFactory(new PropertyValueFactory<>("idNumber"));
             col_date.setCellValueFactory(new PropertyValueFactory<>("date"));
             col_sender.setCellValueFactory(new PropertyValueFactory<>("senderAccountNumber"));
             col_amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
             col_payee.setCellValueFactory(new PropertyValueFactory<>("receiverAccountNumber"));
 
-            transactionsTable.setItems(transactionsList);
+            transactionsTable.setItems(transactionsList); // set the populated observable list to the tableview scene
 
+            // initial filtered list
             FilteredList<TransactionTable> filteredData = new FilteredList<>(transactionsList, b -> true);
-            keywordTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-                filteredData.setPredicate(TransactionTable -> {
+            keywordTextField.textProperty().addListener((observable, oldValue, newValue) -> {   // add listener to capture changes in the search text field
+                filteredData.setPredicate(TransactionTable -> { // set predicate for the filtered list
+
+                    //if no search then display all records
 
                     if (newValue.isEmpty() || newValue.isBlank() || newValue == null) {
                         return true;
                     }
 
-                    String searchKeyword = newValue.toLowerCase();
+                    String searchKeyword = newValue.toLowerCase();  // convert search keyword to lower case
 
-                    if (TransactionTable.getIdNumber().toLowerCase().contains(searchKeyword)) {
+                    if (TransactionTable.getIdNumber().toLowerCase().contains(searchKeyword)) { // check if there are any matches in the column customer Id number to the keyword
                         return true;
-                    } else if (TransactionTable.getSenderAcc().toString().indexOf(searchKeyword) > -1) {
+                    } else if (TransactionTable.getSenderAcc().toString().indexOf(searchKeyword) > -1) { // match in the column sender Account number
                         return true;
-                    } else if (TransactionTable.getPayeeAcc().toString().indexOf(searchKeyword) > -1) {
+                    } else if (TransactionTable.getPayeeAcc().toString().indexOf(searchKeyword) > -1) {  // match in the column receiver Account number
                         return true;
                     } else {
-                        return false; //no match
+                        return false; //no match in any of three columns
                     }
                 });
             });
@@ -107,7 +116,6 @@ public class CustomerTransactionListController implements Initializable {
             e.printStackTrace();
         }
     }
-
 
 }
 
